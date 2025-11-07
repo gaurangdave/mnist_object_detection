e3e428001de5d2490241cfb5d7702e296ac59f9f
Gaurang Dave <gaurangvdave+ubuntu@gmail.com>
2025-11-06 17:50:47 -0800

    ran couple of more experiments - model needs more tweaking

--- src/training_utils.py
+++ src/training_utils.py
@@ -1,10 +1,10 @@
 import keras
 import tensorflow as tf
 
-# helper function to convert box values to corner coordinates
-
 
 def convert_boxes_to_corners(box_center_format):
+    # helper function to convert box values to corner coordinates
+
     # we'll use the following formulas
     # x_min = floor(x_center - (width/2))
     # x_max = floor(x_center + (width/2))
@@ -23,10 +23,10 @@ def convert_boxes_to_corners(box_center_format):
     coordinates = tf.stack(values=[x_min, y_min, x_max, y_max], axis=3)
     return coordinates
 
-# helper function to find the intersection box corners from given 2 boxes
-
 
 def calculate_intersection_corners(box_1_corners, box_2_corners):
+    # helper function to find the intersection box corners from given 2 boxes
+
     x_min_for_intersection = tf.maximum(
         box_1_corners[:, :, :, 0], box_2_corners[:, :, :, 0])
     y_min_for_intersection = tf.maximum(
@@ -39,10 +39,10 @@ def calculate_intersection_corners(box_1_corners, box_2_corners):
         values=[x_min_for_intersection, y_min_for_intersection, x_max_for_intersection, y_max_for_intersection], axis=3)
     return intersection_box_corners
 
-# helper function to calculate the area of intersection between two boxes
-
 
 def calculate_intersection_area(intersection_box_corners):
+    # helper function to calculate the area of intersection between two boxes
+
     # find the width = x_max - x_min, if the boxes are not intersecting, this value could be negative or 0
     intersection_width = tf.maximum(
         0.0, intersection_box_corners[:, :, :, 2] - intersection_box_corners[:, :, :, 0])
@@ -53,26 +53,28 @@ def calculate_intersection_area(intersection_box_corners):
     intersection_area = intersection_width * intersection_height
     return intersection_area
 
-# helper function to calcualte the area of union between two boxes
-
 
 def calculate_union_area(box_1_dimensions, box_2_dimensions, intersection_area):
+    # helper function to calcualte the area of union between two boxes
+
     box_1_area = box_1_dimensions[:, :, :, 0] * box_1_dimensions[:, :, :, 1]
     box_2_area = box_2_dimensions[:, :, :, 0] * box_2_dimensions[:, :, :, 1]
     union_area = box_1_area + box_2_area - intersection_area
     return union_area
 
-# helper function to calculate the IOU ration between boxes
-
 
 def calculate_iou(intersection_area, union_area):
+    # helper function to calculate the IOU ration between boxes
+
     iou = intersection_area / (union_area + 1e-8)
     return iou
 
-# helper function to calculate indices for the grid cells that contain the object
-
 
 def calculate_grid_cell_indices(y_true, y_pred):
+    """
+    Helper function to calculate which grid cell does the ground truth object belong to
+    """
+
     pred_shape = tf.shape(y_pred)
     grid_h = tf.cast(pred_shape[1], dtype=tf.float32)
     grid_w = tf.cast(pred_shape[2], dtype=tf.float32)
@@ -83,6 +85,7 @@ def calculate_grid_cell_indices(y_true, y_pred):
     y_center = y_true[:, :, 2]
 
     # Calculate indices separately
+    # multiplying normalized center by width and height gives us the exact grid cell.
     grid_x_indices = tf.cast(tf.floor(x_center * grid_w), dtype=tf.int32)
     grid_y_indices = tf.cast(tf.floor(y_center * grid_h), dtype=tf.int32)
 
@@ -92,10 +95,10 @@ def calculate_grid_cell_indices(y_true, y_pred):
 
     return grid_indices
 
-# Helper function to calculate anchor box indices.
-
 
 def calculate_anchorbox_indices(y_true, y_pred, grid_cell_indices):
+    # Helper function to calculate anchor box indices.
+
     y_pred_shape = tf.shape(y_pred)
     x_grid_size = y_pred_shape[1]
     y_grid_size = y_pred_shape[2]
@@ -103,8 +106,7 @@ def calculate_anchorbox_indices(y_true, y_pred, grid_cell_indices):
     anchor_boxes = tf.reshape(
         y_pred, shape=(-1, x_grid_size, y_grid_size, 3, 15))
 
-    # # tf.print(f"anchor_boxes.shape {tf.shape(anchor_boxes)}")
-
+    # anchor boxes for grid cells containing ground truth object
     selected_anchor_boxes = tf.gather_nd(
         anchor_boxes, batch_dims=1, indices=grid_cell_indices)
     # # tf.print(f"selected_anchor_boxes.shape :{tf.shape(selected_anchor_boxes)}")
@@ -117,74 +119,58 @@ def calculate_anchorbox_indices(y_true, y_pred, grid_cell_indices):
         expanded_y_true[:, :, :, 1:5])
     y_pred_box_corners = convert_boxes_to_corners(
         selected_anchor_boxes[:, :, :, 1:5])
-    # # tf.print(f"y_true_boxes.shape {y_true_box_corners.shape}")
-    # # tf.print(f"y_pred_boxes.shape {y_pred_box_corners.shape}")
 
     # calculate the intersection coordinates between ground truth and anchor boxes
     intersection_box_corners = calculate_intersection_corners(
         y_true_box_corners[:, :, :, 0:], y_pred_box_corners[:, :, :, 0:])
-    # # tf.print(f"intersection_box_corners.shape {intersection_box_corners.shape}")
 
     # calculate the IOU
     # calculate intersection area
     intersection_area = calculate_intersection_area(intersection_box_corners)
-    # # tf.print(f"intersection_area.shape {intersection_area.shape}")
 
     # calculate union area
     # we just need the width and length for union area
     union_area = calculate_union_area(
         expanded_y_true[:, :, :, 3:5], selected_anchor_boxes[:, :, :, 3:5], intersection_area)
-    # # tf.print(f"union_area.shape {union_area.shape}")
 
     # calculate IOU
     iou = calculate_iou(intersection_area, union_area)
-    # # tf.print(f"iou.shape {iou.shape}")
 
     # select the anchor box based on best iou score
     # select the index with highest iou
     highest_iou_index = tf.argmax(iou, axis=2, output_type=tf.int32)
-    # # tf.print(f"highest_iou_index.shape {highest_iou_index.shape}")
-
     highest_iou_index = tf.expand_dims(highest_iou_index, axis=2)
-    # highest_iou_index = tf.reshape(highest_iou_index, shape=(iou.shape[0],iou.shape[1],-1))
-    # # tf.print(f"expanded highest_iou_index.shape {highest_iou_index.shape}")
     return highest_iou_index
-
-# helper function to calculate best anchor boxes
 
 
 def calculate_best_anchor_boxes(y_true, y_pred):
+    # helper function to calculate best anchor boxes
 
     y_pred_shape = tf.shape(y_pred)
     x_grid_size = y_pred_shape[1]
     y_grid_size = y_pred_shape[2]
 
-    # # tf.print("----- True Values -----")
-    # # tf.print(f"y_true.shape {tf.shape(y_true)}")
-
-    # # tf.print("----- Pred Values -----")
-    # # tf.print(f"y_pred.shape {tf.shape(y_pred)}")
-
     # we have 6x6, each grid cell has 3 anchor box i.e 108 anchor boxes per insantance
     anchor_boxes = tf.reshape(
         y_pred, shape=(-1, x_grid_size, y_grid_size, 3, 15))
-    # # tf.print(f"anchor_boxes.shape {tf.shape(anchor_boxes)}")
+
 
     grid_cell_indices = calculate_grid_cell_indices(
         y_true=y_true, y_pred=y_pred)
 
     # out of 36 grid cells (per instance) select at most 5 grid cells that have ground truth bounding box
+    # 5 grid cells cause at most we'll have 5 digits
     # so out of 108 anchor boxes (per instance) we only need to check 15 anchor boxes
     selected_anchor_boxes = tf.gather_nd(
         anchor_boxes, batch_dims=1, indices=grid_cell_indices)
-    # # tf.print(f"selected_anchor_boxes.shape :{tf.shape(selected_anchor_boxes)}")
+
 
     highest_iou_index = calculate_anchorbox_indices(
         y_true=y_true, y_pred=y_pred, grid_cell_indices=grid_cell_indices)
+    
     # select the anchor box based on the index
     best_anchor_boxes = tf.gather(
         selected_anchor_boxes, indices=highest_iou_index, batch_dims=2)
-    # # tf.print(f"best_anchor_boxes.shape {tf.shape(best_anchor_boxes)}")
 
     return best_anchor_boxes
 
@@ -199,40 +185,22 @@ def calculate_loss(predicted_values, true_values):
     predicted_values_with_objects = tf.boolean_mask(
         predicted_values, mask=objectness_mask)
 
-    # # tf.print(f"true_values_with_objects.shape : {tf.shape(true_values_with_objects)}")
-    # # tf.print(
-    #     f"predicted_values_with_objects.shape : {tf.shape(predicted_values_with_objects)}")
     # slice the 3 properties that we are tyring to calculate loss against
     # predicted values
-
     y_pred_objectness = predicted_values_with_objects[:, 0]
-    # # tf.print(f"y_pred_objectness.shape : {tf.shape(y_pred_objectness)}")
-
     y_pred_bounding_box = predicted_values_with_objects[:, 1:5]
-    # # tf.print(f"y_pred_bounding_box.shape : {tf.shape(y_pred_bounding_box)}")
-
     y_pred_classification = predicted_values_with_objects[:, 5:]
-    # # tf.print(f"y_pred_classification.shape : {tf.shape(y_pred_classification)}")
 
     # True Values
     y_true_objectness = true_values_with_objects[:, 0]
-    # # tf.print(f"y_true_objectness.shape : {tf.shape(y_true_objectness)}")
-
     y_true_bounding_box = true_values_with_objects[:, 1:5]
-    # # tf.print(f"y_true_bounding_box.shape : {tf.shape(y_true_bounding_box)}")
-
     y_true_classification = true_values_with_objects[:, 5:]
-    # # tf.print(f"y_true_classification.shape : {tf.shape(y_true_classification)}")
+
 
     # Apply activation functions to predicted values
     y_pred_objectness = tf.keras.activations.sigmoid(y_pred_objectness)
     y_pred_bounding_box = tf.keras.activations.sigmoid(y_pred_bounding_box)
-    # # tf.print(
-    #     f"Post Activation y_pred_objectness.shape : {tf.shape(y_pred_objectness)}")
-
     y_pred_classification = tf.keras.activations.softmax(y_pred_classification)
-    # # tf.print(
-    #     f"Post Activation y_pred_classification.shape : {tf.shape(y_pred_classification)}")
 
     # Calculate loss
     objectness_loss = tf.keras.losses.BinaryCrossentropy(
@@ -240,7 +208,8 @@ def calculate_loss(predicted_values, true_values):
 
     # bounding_box_loss = tf.keras.losses.MeanSquaredError()(
     #     y_true_bounding_box, y_pred_bounding_box)
-    bounding_box_loss = tf.keras.losses.Huber()(y_true_bounding_box, y_pred_bounding_box)
+    bounding_box_loss = tf.keras.losses.Huber()(
+        y_true_bounding_box, y_pred_bounding_box)
 
     classification_loss = tf.keras.losses.CategoricalCrossentropy()(
         y_true_classification, y_pred_classification)
@@ -440,14 +409,8 @@ def calculate_model_loss_dict(y_true, y_pred, lambdas):
     objectless_loss = calculate_objectless_loss(
         y_true=y_true, y_pred=y_pred)
 
-    # scale the losses
-    # lambda_objectness = 1
-    # lambda_bounding_box = 0.001
-    # lambda_classification = 1
-    # lambda_objectless = 1
-
     total_loss = (objectness_loss * lambdas["obj"]) + (bounding_box_loss *
-                                                          lambdas["bbox"]) + (classification_loss * lambdas["class"]) + (objectless_loss * lambdas["obj_less"])
+                                                       lambdas["bbox"]) + (classification_loss * lambdas["class"]) + (objectless_loss * lambdas["obj_less"])
 
     return {
         "loss": total_loss,
